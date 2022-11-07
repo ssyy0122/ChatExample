a5c8339ff900d4d91a52b9568cb49b7fd0b2ed62
ssyy0122 <z01039118740@gmail.com>
2022-11-07 22:31:24 +0900

    ChatHandler 구현

--- src/main/java/com/example/chatexample/Config/WebSocketConfig.java
+++ src/main/java/com/example/chatexample/Config/WebSocketConfig.java
@@ -1,17 +1,20 @@
 package com.example.chatexample.Config;
 
 import com.example.chatexample.Handler.ChatHandler;
-import com.sun.xml.txw2.output.CharacterEscapeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired
     ChatHandler chatHandler;
+    @Override
+    public void registerWebSocketHandler(WebSocketHandlerRegistry registry){
 
+        registry.addHandler(chatHandler,"ws/Chat").setAllowedOrigins("*");
+    }
 }
--- src/main/java/com/example/chatexample/Handler/ChatHandler.java
+++ src/main/java/com/example/chatexample/Handler/ChatHandler.java
@@ -1,4 +1,38 @@
 package com.example.chatexample.Handler;
 
-public class ChatHandler {
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ChatHandler extends TextWebSocketHandler {
+
+    private static List<WebSocketSession>list = new ArrayList<>();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message ) throws Exception {
+        String payload = message.getPayload();
+        System.out.printf("payload:" + payload);
+
+        for (WebSocketSession sess : list) {
+            sess.sendMessage(message);
+        }
+    }
+    //  client가 접속 시 호출되는 메서드
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        list.add(session);
+
+        System.out.printf(session + "클라이언트 접속");
+    }
+    // Client가 접속 해제 될 시 호출 되는 메서드
+    public void afterConnectionClosed(WebSocketSession session , CloseStatus status) throws Exception {
+        System.out.printf(session + "클라이언트 접속 헤제");
+        list.remove(session);
+    }
 }
